a09804e6a29a2be50aa71646d431b560eed663b5
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-27 09:38:41 +0100

    basic fwd fn

--- nn.cpp
+++ nn.cpp
@@ -1,5 +1,7 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 float random() {
   return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
@@ -38,23 +40,38 @@ class rfa {
 public:
   rfa() {
     for (auto &f : m_data)
-      f = random();
+      f = random() * 10.0f - 5.0f;
   }
   rfa(const float (&n)[2]) {
     for (auto i = 0; i < 2; i++) {
       m_data[i] = n[i];
     }
   }
+
+  auto operator[](unsigned idx) const noexcept { return m_data[idx]; }
 };
 class neuron {
   rfa m_w{};
   rfa m_b{};
 
+  static float sigm(float a) {
+    float ea = expf(a);
+    return ea / (ea + 1.0f);
+  }
+
 public:
-  float fwd(const rfa &in) const { return 1.20; }
+  float fwd(const rfa &in) const {
+    float a = 0;
+    for (auto i = 0; i < 2; i++) {
+      a += m_w[i] * in[i] + m_b[i];
+    }
+    return sigm(a);
+  }
 };
 
 int main() {
+  srand(time(0));
+
   neuron n;
 
   rfa in{or_data.data[0].in};
