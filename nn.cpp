05bcb9afda992e6596451583a80d2c865a6709a4
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-27 08:56:06 +0100

    test data

--- nn.cpp
+++ nn.cpp
@@ -1,1 +1,28 @@
+struct test {
+  float in[2];
+  float out[1];
+};
+struct test_suit {
+  test data[4];
+};
+
+constexpr const test_suit or_data = {{
+    {{0, 0}, {0}},
+    {{0, 1}, {1}},
+    {{1, 0}, {1}},
+    {{1, 1}, {1}},
+}};
+constexpr const test_suit and_data = {{
+    {{0, 0}, {0}},
+    {{0, 1}, {0}},
+    {{1, 0}, {0}},
+    {{1, 1}, {1}},
+}};
+constexpr const test_suit xor_data = {{
+    {{0, 0}, {0}},
+    {{0, 1}, {1}},
+    {{1, 0}, {1}},
+    {{1, 1}, {0}},
+}};
+
 int main() {}
