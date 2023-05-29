80d2495c8f0941e6c60dc83139b033971eb42844
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-29 10:21:51 +0100

    seeding rng

--- nn.cpp
+++ nn.cpp
@@ -1,13 +1,11 @@
 import bestpair;
+import rng;
 import testdata;
-
-#include <stdlib.h>
-#include <time.h>
 
 int main() {
   static constexpr const auto max_gens = 1000;
 
-  srand(time(0));
+  rng::seed();
 
   best_pair_algo{}.survive(or_data, max_gens);
   best_pair_algo{}.survive(and_data, max_gens);
