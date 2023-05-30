25875f40764be3fbb3f4cc3a61890386ea3679c9
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-30 09:56:14 +0100

    reading test data from stdin

--- nn.cpp
+++ nn.cpp
@@ -1,17 +1,21 @@
+#include <stdio.h>
+
 import algo;
 import rng;
 import testdata;
 
 int main() {
   static constexpr const auto max_gens = 1000;
-  using alg = algo::weightned;
+
+  test_suit data{};
+
+  for (unsigned i = 0; i < 4; i++) {
+    for (unsigned j = 0; j < 2; j++) {
+      scanf("%f,", &data.data[i].in[j]);
+    }
+    scanf("%f\n", &data.data[i].out[0]);
+  }
 
   rng::seed();
-
-  (new alg())->survive(or_data, max_gens);
-  (new alg())->survive(and_data, max_gens);
-
-  alg *p = new alg{};
-  p->survive(xor_data, max_gens);
-  p->survive(xor_data, max_gens);
+  (new algo::weightned())->survive(data, max_gens);
 }
--- testdata.cppm
+++ testdata.cppm
@@ -7,22 +7,3 @@ export struct test {
 export struct test_suit {
   test data[4];
 };
-
-export constexpr const test_suit or_data = {{
-    {{0, 0}, {0}},
-    {{0, 1}, {1}},
-    {{1, 0}, {1}},
-    {{1, 1}, {1}},
-}};
-export constexpr const test_suit and_data = {{
-    {{0, 0}, {0}},
-    {{0, 1}, {0}},
-    {{1, 0}, {0}},
-    {{1, 1}, {1}},
-}};
-export constexpr const test_suit xor_data = {{
-    {{0, 0}, {0}},
-    {{0, 1}, {1}},
-    {{1, 0}, {1}},
-    {{1, 1}, {0}},
-}};
